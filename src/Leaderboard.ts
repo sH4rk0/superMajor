4e49e602f58e1248b4b7b11504ed666352be3ae0
FRA-RHW <francesco.raimondo@healthwareinternational.com>
2019-05-27 09:34:34 +0200

    removed keys

--- src/Leaderboard.ts
+++ src/Leaderboard.ts
@@ -2,13 +2,13 @@ import * as firebase from 'firebase'
 
 export default class Leaderboard {
   private firebaseConfig = {
-    apiKey: 'AIzaSyAdRVmgt6jh-CJ4nmZQh8KwNp_ABivgdmo',
-    authDomain: 'supermajor2k19.firebaseapp.com',
-    databaseURL: 'https://supermajor2k19.firebaseio.com',
-    projectId: 'supermajor2k19',
-    storageBucket: 'supermajor2k19.appspot.com',
-    messagingSenderId: '789535657299',
-    appId: '1:789535657299:web:ce6ba1fd68a249b1'
+    apiKey: '****',
+    authDomain: '****',
+    databaseURL: '****',
+    projectId: '****',
+    storageBucket: '****',
+    messagingSenderId: '****',
+    appId: '****'
   }
   private fireBaseApp: firebase.app.App
   private fireBaseDb: firebase.database.Database
